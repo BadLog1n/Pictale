edb2555d8c21306e9151ee759a0bf8b17aba3763
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2023-01-10 11:20:04 +0300

    Реализованы фрагменты

--- app/src/main/java/com/oneseed/pictale/MainActivity.kt
+++ app/src/main/java/com/oneseed/pictale/MainActivity.kt
@@ -2,10 +2,17 @@ package com.oneseed.pictale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.oneseed.pictale.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_content)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/oneseed/pictale/SearchFragment.kt
@@ -0,0 +1,31 @@
+package com.oneseed.pictale
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.navArgs
+import com.oneseed.pictale.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
