51c767ce7cc3f36a818894b056375ef29377c401
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2023-01-10 12:07:47 +0300

    Реализовано переключение между фрагментами

--- app/src/main/java/com/oneseed/pictale/ContentFragment.kt
+++ app/src/main/java/com/oneseed/pictale/ContentFragment.kt
@@ -5,20 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.oneseed.pictale.databinding.FragmentContentBinding
 
 
 class ContentFragment : Fragment() {
+    private lateinit var binding: FragmentContentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false)
-
+        binding = FragmentContentBinding.inflate(layoutInflater, container, false)
+        return binding.root
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backAction.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
 
 
 }
--- app/src/main/java/com/oneseed/pictale/SearchFragment.kt
+++ app/src/main/java/com/oneseed/pictale/SearchFragment.kt
@@ -25,7 +25,10 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.submit.setOnClickListener{
 
+            view.findNavController().navigate(R.id.contentFragment)
+        }
     }
 
 }
