1b6f306cc9270fa965b02e984eacf4bc74e439cc
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2023-01-11 08:52:37 +0300

    Добавлено отображение текста из базы данных

--- app/src/main/java/com/oneseed/pictale/ContentFragment.kt
+++ app/src/main/java/com/oneseed/pictale/ContentFragment.kt
@@ -1,5 +1,7 @@
 package com.oneseed.pictale
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -23,11 +25,28 @@ class ContentFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        displayValue()
+
         super.onViewCreated(view, savedInstanceState)
+
         binding.backAction.setOnClickListener {
             findNavController().navigateUp()
         }
     }
 
+    private fun displayValue(){
+        val sharedPref: SharedPreferences? = activity?.getSharedPreferences(
+            getString(R.string.sharedPref), Context.MODE_PRIVATE
+        )
+        sharedPref?.getString(getString(R.string.titleValue), null)?.let {
+            binding.titleText.text = it
+        }
+        sharedPref?.getString(getString(R.string.mainTextValue), null)?.let {
+            binding.mainText.text = it
+        }
+        sharedPref?.getString(getString(R.string.emailTextValue), null)?.let {
+            binding.emailContactText.text = it
+        }
+    }
 
 }
--- app/src/main/java/com/oneseed/pictale/SearchFragment.kt
+++ app/src/main/java/com/oneseed/pictale/SearchFragment.kt
@@ -1,5 +1,7 @@
 package com.oneseed.pictale
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.oneseed.pictale.databinding.FragmentSearchBinding
@@ -15,6 +18,7 @@ import com.oneseed.pictale.databinding.FragmentSearchBinding
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private lateinit var database: DatabaseReference
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -25,18 +29,37 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         database = FirebaseDatabase.getInstance().getReference("key")
         binding.submit.setOnClickListener {
             val inputCode = binding.codeInputText.text.toString()
-            if (inputCode.isNotEmpty()){
+            if (inputCode.isNotEmpty()) {
                 database.child(inputCode).get().addOnSuccessListener {
-                    if (it.exists()) view.findNavController().navigate(R.id.contentFragment)
-                    else Toast.makeText(requireContext(), "Код не найден", Toast.LENGTH_SHORT).show()
+                    if (it.exists()) {
+                        getAllData(it)
+                        view.findNavController().navigate(R.id.contentFragment)
+                    } else Toast.makeText(requireContext(), "Код не найден", Toast.LENGTH_SHORT)
+                        .show()
                 }
-            }
-            else Toast.makeText(requireContext(), "Код не может быть пустым", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(requireContext(), "Код не может быть пустым", Toast.LENGTH_SHORT)
+                .show()
 
         }
 
+    }
+
+    private fun getAllData(it: DataSnapshot) {
+        val sharedPref: SharedPreferences? = activity?.getSharedPreferences(
+            getString(R.string.sharedPref), Context.MODE_PRIVATE
+        )
+        sharedPref?.edit()?.putString(
+            getString(R.string.titleValue), it.child("title").value.toString()
+        )?.apply()
+        sharedPref?.edit()?.putString(
+            getString(R.string.mainTextValue), it.child("mainText").value.toString()
+        )?.apply()
+        sharedPref?.edit()?.putString(
+            getString(R.string.emailTextValue), it.child("email").value.toString()
+        )?.apply()
     }
 }
