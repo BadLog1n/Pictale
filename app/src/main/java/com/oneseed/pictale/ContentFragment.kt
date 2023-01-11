956355bcc036544681bf0f04c9ba944b4adfb9cc
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2023-01-11 09:11:20 +0300

    Добавлено отображение основного изображения
    Изображение динамически подгружается из базы данных

--- app/src/main/java/com/oneseed/pictale/ContentFragment.kt
+++ app/src/main/java/com/oneseed/pictale/ContentFragment.kt
@@ -2,17 +2,26 @@ package com.oneseed.pictale
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.oneseed.pictale.databinding.FragmentContentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class ContentFragment : Fragment() {
     private lateinit var binding: FragmentContentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,7 +43,7 @@ class ContentFragment : Fragment() {
         }
     }
 
-    private fun displayValue(){
+    private fun displayValue() {
         val sharedPref: SharedPreferences? = activity?.getSharedPreferences(
             getString(R.string.sharedPref), Context.MODE_PRIVATE
         )
@@ -46,6 +55,19 @@ class ContentFragment : Fragment() {
         }
         sharedPref?.getString(getString(R.string.emailTextValue), null)?.let {
             binding.emailContactText.text = it
+        }
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val imageRef = Firebase.storage.reference
+                val maxDownloadSize = 5L * 1024 * 1024 * 1024
+                val bytes = imageRef.child("1/1.jpg").getBytes(maxDownloadSize).await()
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                binding.mainImage.setImageBitmap(bmp)
+                binding.toImgProgress.visibility = View.GONE
+            } catch (_: Exception) {
+            }
         }
     }
 
